e45ccd74ef0001686ae2a241bad28477e20a02be
Tamas Cservenak <tamas@cservenak.net>
2012-11-16 13:18:07 +0100

    Updated for Maven Indexer 5.1.0

--- indexer-example-01/src/main/java/org/apache/maven/indexer/example/App.java
+++ indexer-example-01/src/main/java/org/apache/maven/indexer/example/App.java
@@ -22,10 +22,10 @@ import org.apache.maven.index.FlatSearchResponse;
 import org.apache.maven.index.GroupedSearchRequest;
 import org.apache.maven.index.GroupedSearchResponse;
 import org.apache.maven.index.Grouping;
+import org.apache.maven.index.Indexer;
 import org.apache.maven.index.IteratorSearchRequest;
 import org.apache.maven.index.IteratorSearchResponse;
 import org.apache.maven.index.MAVEN;
-import org.apache.maven.index.NexusIndexer;
 import org.apache.maven.index.context.IndexCreator;
 import org.apache.maven.index.context.IndexUtils;
 import org.apache.maven.index.context.IndexingContext;
@@ -42,6 +42,9 @@ import org.apache.maven.wagon.events.TransferEvent;
 import org.apache.maven.wagon.events.TransferListener;
 import org.apache.maven.wagon.observers.AbstractTransferListener;
 import org.codehaus.plexus.DefaultPlexusContainer;
+import org.codehaus.plexus.PlexusContainer;
+import org.codehaus.plexus.PlexusContainerException;
+import org.codehaus.plexus.component.repository.exception.ComponentLookupException;
 import org.codehaus.plexus.util.StringUtils;
 import org.sonatype.aether.util.version.GenericVersionScheme;
 import org.sonatype.aether.version.InvalidVersionSpecificationException;
@@ -52,30 +55,58 @@ import org.sonatype.aether.version.Version;
  */
 public class App
 {
-
     public static void main( String[] args )
         throws Exception
     {
-        // create Plexus IoC (actually SISU-plexus compat)
-        DefaultPlexusContainer plexus = new DefaultPlexusContainer();
+        final App app = new App();
+        app.perform();
+    }
 
+    // ==
+
+    private final PlexusContainer plexusContainer;
+
+    private final Indexer indexer;
+
+    private final IndexUpdater indexUpdater;
+
+    private final Wagon httpWagon;
+
+    public App()
+        throws PlexusContainerException, ComponentLookupException
+    {
+        // here we create Plexus container, the Maven default IoC container
+        // Plexus falls outside of MI scope, just accept the fact that
+        // MI is a Plexus component ;)
+        // If needed more info, ask on Maven Users list or Plexus Users list
+        // google is your friend!
+        this.plexusContainer = new DefaultPlexusContainer();
+
+        // lookup the indexer components from plexus
+        this.indexer = plexusContainer.lookup( Indexer.class );
+        this.indexUpdater = plexusContainer.lookup( IndexUpdater.class );
+        // lookup wagon used to remotely fetch index
+        this.httpWagon = plexusContainer.lookup( Wagon.class, "http" );
+
+    }
+
+    public void perform()
+        throws IOException, ComponentLookupException, InvalidVersionSpecificationException
+    {
         // Files where local cache is (if any) and Lucene Index should be located
         File centralLocalCache = new File( "target/central-cache" );
         File centralIndexDir = new File( "target/central-index" );
 
         // Creators we want to use (search for fields it defines)
         List<IndexCreator> indexers = new ArrayList<IndexCreator>();
-        indexers.add( plexus.lookup( IndexCreator.class, "min" ) );
-        indexers.add( plexus.lookup( IndexCreator.class, "jarContent" ) );
-        indexers.add( plexus.lookup( IndexCreator.class, "maven-plugin" ) );
-
-        // lookup the indexer instance from plexus
-        NexusIndexer nexusIndexer = plexus.lookup( NexusIndexer.class );
+        indexers.add( plexusContainer.lookup( IndexCreator.class, "min" ) );
+        indexers.add( plexusContainer.lookup( IndexCreator.class, "jarContent" ) );
+        indexers.add( plexusContainer.lookup( IndexCreator.class, "maven-plugin" ) );
 
         // Create context for central repository index
         IndexingContext centralContext =
-            nexusIndexer.addIndexingContextForced( "central-context", "central", centralLocalCache, centralIndexDir,
-                "http://repo1.maven.org/maven2", null, indexers );
+            indexer.createIndexingContext( "central-context", "central", centralLocalCache, centralIndexDir,
+                                           "http://repo1.maven.org/maven2", null, true, true, indexers );
 
         // Update the index (incremental update will happen if this is not 1st run and files are not deleted)
         // This whole block below should not be executed on every app start, but rather controlled by some configuration
@@ -88,7 +119,6 @@ public class App
             System.out.println( "This might take a while on first run, so please be patient!" );
             // Create ResourceFetcher implementation to be used with IndexUpdateRequest
             // Here, we use Wagon based one as shorthand, but all we need is a ResourceFetcher implementation
-            Wagon wagon = plexus.lookup( Wagon.class, "http" );
             TransferListener listener = new AbstractTransferListener()
             {
                 public void transferStarted( TransferEvent transferEvent )
@@ -105,12 +135,11 @@ public class App
                     System.out.println( " - Done" );
                 }
             };
-            ResourceFetcher resourceFetcher = new WagonHelper.WagonFetcher( wagon, listener, null, null );
+            ResourceFetcher resourceFetcher = new WagonHelper.WagonFetcher( httpWagon, listener, null, null );
 
             Date centralContextCurrentTimestamp = centralContext.getTimestamp();
             IndexUpdateRequest updateRequest = new IndexUpdateRequest( centralContext, resourceFetcher );
-            IndexUpdater updater = plexus.lookup( IndexUpdater.class );
-            IndexUpdateResult updateResult = updater.fetchAndUpdateIndex( updateRequest );
+            IndexUpdateResult updateResult = indexUpdater.fetchAndUpdateIndex( updateRequest );
             if ( updateResult.isFullUpdate() )
             {
                 System.out.println( "Full update happened!" );
@@ -122,7 +151,7 @@ public class App
             else
             {
                 System.out.println( "Incremental update happened, change covered " + centralContextCurrentTimestamp
-                    + " - " + updateResult.getTimestamp() + " period." );
+                                        + " - " + updateResult.getTimestamp() + " period." );
             }
 
             System.out.println();
@@ -150,7 +179,7 @@ public class App
                         final Document doc = ir.document( i );
                         final ArtifactInfo ai = IndexUtils.constructArtifactInfo( doc, centralContext );
                         System.out.println( ai.groupId + ":" + ai.artifactId + ":" + ai.version + ":" + ai.classifier
-                            + " (sha1=" + ai.sha1 + ")" );
+                                                + " (sha1=" + ai.sha1 + ")" );
                     }
                 }
             }
@@ -170,19 +199,19 @@ public class App
 
         // construct the query for known GA
         final Query groupIdQ =
-            nexusIndexer.constructQuery( MAVEN.GROUP_ID, new SourcedSearchExpression( "org.sonatype.nexus" ) );
+            indexer.constructQuery( MAVEN.GROUP_ID, new SourcedSearchExpression( "org.sonatype.nexus" ) );
         final Query artifactIdQ =
-            nexusIndexer.constructQuery( MAVEN.ARTIFACT_ID, new SourcedSearchExpression( "nexus-api" ) );
+            indexer.constructQuery( MAVEN.ARTIFACT_ID, new SourcedSearchExpression( "nexus-api" ) );
         final BooleanQuery query = new BooleanQuery();
         query.add( groupIdQ, Occur.MUST );
         query.add( artifactIdQ, Occur.MUST );
 
         // we want "jar" artifacts only
-        query.add( nexusIndexer.constructQuery( MAVEN.PACKAGING, new SourcedSearchExpression( "jar" ) ), Occur.MUST );
+        query.add( indexer.constructQuery( MAVEN.PACKAGING, new SourcedSearchExpression( "jar" ) ), Occur.MUST );
         // we want main artifacts only (no classifier)
         // Note: this below is unfinished API, needs fixing
-        query.add( nexusIndexer.constructQuery( MAVEN.CLASSIFIER, new SourcedSearchExpression( Field.NOT_PRESENT ) ),
-            Occur.MUST_NOT );
+        query.add( indexer.constructQuery( MAVEN.CLASSIFIER, new SourcedSearchExpression( Field.NOT_PRESENT ) ),
+                   Occur.MUST_NOT );
 
         // construct the filter to express "V greater than"
         final ArtifactInfoFilter versionFilter = new ArtifactInfoFilter()
@@ -204,7 +233,7 @@ public class App
         };
 
         final IteratorSearchRequest request = new IteratorSearchRequest( query, versionFilter );
-        final IteratorSearchResponse response = nexusIndexer.searchIterator( request );
+        final IteratorSearchResponse response = indexer.searchIterator( request );
         for ( ArtifactInfo ai : response )
         {
             System.out.println( ai.toString() );
@@ -214,14 +243,15 @@ public class App
         // Use index
         // Searching for some artifact
         Query gidQ =
-            nexusIndexer.constructQuery( MAVEN.GROUP_ID, new SourcedSearchExpression( "org.apache.maven.indexer" ) );
-        Query aidQ = nexusIndexer.constructQuery( MAVEN.ARTIFACT_ID, new SourcedSearchExpression( "indexer-artifact" ) );
+            indexer.constructQuery( MAVEN.GROUP_ID, new SourcedSearchExpression( "org.apache.maven.indexer" ) );
+        Query aidQ =
+            indexer.constructQuery( MAVEN.ARTIFACT_ID, new SourcedSearchExpression( "indexer-artifact" ) );
 
         BooleanQuery bq = new BooleanQuery();
         bq.add( gidQ, Occur.MUST );
         bq.add( aidQ, Occur.MUST );
 
-        searchAndDump( nexusIndexer, "all artifacts under GA org.apache.maven.indexer:indexer-artifact", bq );
+        searchAndDump( indexer, "all artifacts under GA org.apache.maven.indexer:indexer-artifact", bq );
 
         // Searching for some main artifact
         bq = new BooleanQuery();
@@ -230,33 +260,35 @@ public class App
         // bq.add( nexusIndexer.constructQuery( MAVEN.CLASSIFIER, new SourcedSearchExpression( "*" ) ), Occur.MUST_NOT
         // );
 
-        searchAndDump( nexusIndexer, "main artifacts under GA org.apache.maven.indexer:indexer-artifact", bq );
+        searchAndDump( indexer, "main artifacts under GA org.apache.maven.indexer:indexer-artifact", bq );
 
         // doing sha1 search
-        searchAndDump( nexusIndexer, "SHA1 7ab67e6b20e5332a7fb4fdf2f019aec4275846c2", nexusIndexer.constructQuery(
+        searchAndDump( indexer, "SHA1 7ab67e6b20e5332a7fb4fdf2f019aec4275846c2", indexer.constructQuery(
             MAVEN.SHA1, new SourcedSearchExpression( "7ab67e6b20e5332a7fb4fdf2f019aec4275846c2" ) ) );
 
-        searchAndDump( nexusIndexer, "SHA1 7ab67e6b20 (partial hash)",
-            nexusIndexer.constructQuery( MAVEN.SHA1, new UserInputSearchExpression( "7ab67e6b20" ) ) );
+        searchAndDump( indexer, "SHA1 7ab67e6b20 (partial hash)",
+                       indexer.constructQuery( MAVEN.SHA1, new UserInputSearchExpression( "7ab67e6b20" ) ) );
 
         // doing classname search (incomplete classname)
-        searchAndDump( nexusIndexer, "classname DefaultNexusIndexer (note: Central does not publish classes in the index)",
-            nexusIndexer.constructQuery( MAVEN.CLASSNAMES, new UserInputSearchExpression( "DefaultNexusIndexer" ) ) );
+        searchAndDump( indexer,
+                       "classname DefaultNexusIndexer (note: Central does not publish classes in the index)",
+                       indexer.constructQuery( MAVEN.CLASSNAMES,
+                                               new UserInputSearchExpression( "DefaultNexusIndexer" ) ) );
 
         // doing search for all "canonical" maven plugins latest versions
         bq = new BooleanQuery();
-        bq.add( nexusIndexer.constructQuery( MAVEN.PACKAGING, new SourcedSearchExpression( "maven-plugin" ) ),
-            Occur.MUST );
+        bq.add( indexer.constructQuery( MAVEN.PACKAGING, new SourcedSearchExpression( "maven-plugin" ) ),
+                Occur.MUST );
         bq.add(
-            nexusIndexer.constructQuery( MAVEN.GROUP_ID, new SourcedSearchExpression( "org.apache.maven.plugins" ) ),
+            indexer.constructQuery( MAVEN.GROUP_ID, new SourcedSearchExpression( "org.apache.maven.plugins" ) ),
             Occur.MUST );
-        searchGroupedAndDump( nexusIndexer, "all \"canonical\" maven plugins", bq, new GAGrouping() );
+        searchGroupedAndDump( indexer, "all \"canonical\" maven plugins", bq, new GAGrouping() );
 
         // close cleanly
-        nexusIndexer.removeIndexingContext( centralContext, false );
+        indexer.closeIndexingContext( centralContext, false );
     }
 
-    public static void searchAndDump( NexusIndexer nexusIndexer, String descr, Query q )
+    public static void searchAndDump( Indexer nexusIndexer, String descr, Query q )
         throws IOException
     {
         System.out.println( "Searching for " + descr );
@@ -273,7 +305,7 @@ public class App
         System.out.println();
     }
 
-    public static void searchGroupedAndDump( NexusIndexer nexusIndexer, String descr, Query q, Grouping g )
+    public static void searchGroupedAndDump( Indexer nexusIndexer, String descr, Query q, Grouping g )
         throws IOException
     {
         System.out.println( "Searching for " + descr );
@@ -286,7 +318,7 @@ public class App
             System.out.println( "* Plugin " + ai.artifactId );
             System.out.println( "  Latest version:  " + ai.version );
             System.out.println( StringUtils.isBlank( ai.description ) ? "No description in plugin's POM."
-                : StringUtils.abbreviate( ai.description, 60 ) );
+                                    : StringUtils.abbreviate( ai.description, 60 ) );
             System.out.println();
         }
 
